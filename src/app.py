d41f867fab85eeaa4203471d3dc5dbaf9c012db3
annunezc <andresnunezcelis@gmail.com>
2025-01-14 01:33:25 +0000

    bases sql

--- src/app.py
+++ src/app.py
@@ -8,9 +8,16 @@ load_dotenv()
 
 # 1) Connect to the database here using the SQLAlchemy's create_engine function
 
-connection_string = f"postgresql://{os.getenv('gitpod')}:{os.getenv('postgres')}@{os.getenv('localhost')}/{os.getenv('sample_db')}"
+connection_string = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
 engine = create_engine(connection_string).execution_options(autocommit=True)
 engine.connect()
+db=engine.connect()
+engine.execute("""DROP TABLE book_authors;
+               DROP TABLE books;
+               DROP TABLE authors;
+               DROP TABLE publishers;
+               """)
+
 
 # 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
 engine.execute(""" CREATE TABLE publishers(
